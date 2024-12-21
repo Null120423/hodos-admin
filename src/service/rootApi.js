ca3b572b18cfea879acdad079339729972578dab
huutai <huutt201@example.com>
2024-12-21 16:29:34 +0700

    root api

--- src/service/rootApi.js
+++ src/service/rootApi.js
@@ -1,5 +1,5 @@
 import initApi from './initApi';
 
 const api = [import.meta.env.REACT_APP_BACKEND_URL];
-const rootApi = initApi('http://[::1]:3000');
+const rootApi = initApi('https://hodos-hackathon.genny.id.vn');
 export default rootApi;

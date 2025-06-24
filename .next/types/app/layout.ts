549353907264be67c0d364200392d9a22ee1a270
huutai <huutt201@example.com>
2025-06-24 11:31:33 +0700

    demo

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Volumes/T7/hodos-admin/app/layout.tsx
+// File: /Users/genny/WORK/HODOS/hodos-admin/app/layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Volumes/T7/hodos-admin/app/page.tsx
+// File: /Users/genny/WORK/HODOS/hodos-admin/app/page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 

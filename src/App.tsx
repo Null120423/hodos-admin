759a1f4fed102cc90e5890d4cff488685aa8548f
huutai <huutt201@example.com>
2025-07-02 08:04:08 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,23 @@ function App() {
 
           // Alias Token
           colorBgContainer: "#fff",
+
+          // Seed Token
+          // colorPrimary: "#1B5335",
+          // borderRadius: 4,
+
+          // // Alias Token
+          // colorBgContainer: "#CDCFC3",
+          // colorBgBase: "#CDCFC3",
+          // colorText: "#1E1D1D",
+          // colorBgLayout: "#F2E8E0",
+          // colorBgSolidActive: "#F2E8E0",
+          // colorBgElevated: "#F2E8E0",
+          // colorTextHeading: "#1E1D1D",
+          // colorTextBase: "#1E1D1D",
+          // colorTextDescription: "#1E1D1D",
+          // colorTextLightSolid: "#1E1D1D",
+          // colorTextLabel: "#fff",
         },
       }}
     >
